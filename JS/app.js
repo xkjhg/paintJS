fd0d504a819ab4f4b045c7257ecd7bc2c4514433
xkjhg <xkjhg@naver.com>
2021-08-09 19:43:11 +0900

    7th commit

--- JS/app.js
+++ JS/app.js
@@ -4,6 +4,8 @@ const colors = document.getElementsByClassName("jsColor")
 const range = document.getElementById("jsRange")
 const mode = document.getElementById("jsMode")
 const saveBtn = document.getElementById("jsSave")
+const square = document.getElementById("jsSquare")
+const circle = document.getElementById("jsCircle")
 
 const INITIAL_COLOR = "#2c2c2c"
 
@@ -19,6 +21,7 @@ ctx.lineWidth = 2.5
 
 let painting = false
 let filling = false
+let paintSquare = false
 
 function startPainting() {
   painting = true
@@ -76,6 +79,16 @@ function handleSaveClick() {
   link.click()
 }
 
+function handlesquareClick() {
+  if (paintSquare === true) {
+    paintSquare = false
+    square.innerText = "□"
+  } else {
+    paintSquare = true
+    square.innerText = "■"
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove)
   canvas.addEventListener("mousedown", startPainting)
@@ -96,4 +109,8 @@ if (mode) {
 
 if (saveBtn) {
   saveBtn.addEventListener("click", handleSaveClick)
+}
+
+if (square) {
+  square.addEventListener("click", handlesquareClick)
 }
